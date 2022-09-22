28e9c60a183864d0b6c2877c7861d7dbdd858782
Yuliia Hlushko <glushkoyulia9992@gmail.com>
2022-09-22 11:39:44 +0300

    )

--- task.cs
+++ task.cs
@@ -1,7 +1,7 @@
 var groupName = "IA-04";
-var orderNumber = 12;
-var name = "Borys";
-var surname = "Kovba";
-var greetings = "Aloha";
+var orderNumber = 6;
+var name = "Yuliia";
+var surname = "Hlushko";
+var greetings = "Hi!!";
 
 System.Console.WriteLine("{0}, {1} {2}, {3}.{4}", greetings, name, surname, groupName, orderNumber);
