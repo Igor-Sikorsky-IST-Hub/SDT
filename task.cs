593077b23b2d251197197d8643bb2eec957e1ecc
bohdan <bogdan2003bk96@gmail.com>
2022-09-22 11:03:47 +0300

    init

--- task.cs
+++ task.cs
@@ -1,7 +1,7 @@
-var groupName = "IA-XX";
+var groupName = "IA-04";
 var orderNumber = -1;
-var name = "First Name";
-var surname = "Last Name";
+var name = "Bohdan";
+var surname = "Karashchuk";
 var greetings = "Hello world";
 
 System.Console.WriteLine("{0}, {1} {2}, {3}.{4}", greetings, name, surname, groupName, orderNumber);
