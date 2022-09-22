5755ec5d09e176d9e48e469b68c07d999e4fbba2
Oleg Khalanskyi <okhalanskyi@gmail.com>
2022-09-22 12:17:34 +0300

    cs

--- task.R
+++
@@ -1,8 +0,0 @@
-group_number <- "IA-04"
-order_number <- "24"
-
-name <- "Oleg"
-surname <- "Khalanskyi"
-greetings <- "Hello World"
-
-paste(greetings, order_number, name, surname)
---
+++ task.cs
@@ -0,0 +1,7 @@
+var groupName = "IA-04";
+var orderNumber = 24;
+var name = "Oleg";
+var surname = "Khalanskyi";
+var greetings = "Hi";
+
+System.Console.WriteLine("{0}, {1} {2}, {3}.{4}", greetings, name, surname, groupName, orderNumber);
