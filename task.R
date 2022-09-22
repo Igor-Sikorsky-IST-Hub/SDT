d764bc4e27142ad00bd4c35d6801086f1286b956
July58 <perevozniukjv@gmail.com>
2022-09-22 10:03:46 +0300

    Perevozniuk

--- task.R
+++ task.R
@@ -1,8 +1,8 @@
 group_number <- "IA-01"
-order_number <- "17"
+order_number <- "22"
 
-name <- "Daniil"
-surname <- "Zhyhorin"
+name <- "Yuliia"
+surname <- "Perevozniuk"
 greetings <- "Hello World"
 
 paste(greetings, order_number, name, surname)
