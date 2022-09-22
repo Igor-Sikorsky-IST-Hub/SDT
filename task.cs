74ad9b2d46b590c70b56145714484bea10a34c0c
Yuliia Hlushko <78353295+Panda913@users.noreply.github.com>
2022-09-22 12:14:55 +0300

    Update task.cs

--- task.cs
+++ task.cs
@@ -1,7 +1,7 @@
-var orderNumber = 1;
-var name = "Anton";
-var surname = "Antoniuk";
-var greetings = "Kupliu Belgorod za BigMac";
->>>>>>> Antoniuk_Anton
+var orderNumber = 6;
+var name = "Yuliia";
+var surname = "Hlushko";
+var greetings = "Hi";
+
 
 System.Console.WriteLine("{0}, {1} {2}, {3}.{4}", greetings, name, surname, groupName, orderNumber);
