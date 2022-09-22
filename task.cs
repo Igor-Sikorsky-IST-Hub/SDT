d594d7c720a423ae8500e1f5305f9bd92dc6291a
varCODEx <borys.kovba@gmail.com>
2022-09-22 11:14:04 +0300

    add & modify my branch

--- task.cs
+++ task.cs
@@ -1,7 +1,7 @@
-var groupName = "IA-XX";
-var orderNumber = -1;
-var name = "First Name";
-var surname = "Last Name";
-var greetings = "Hello world";
+var groupName = "IA-04";
+var orderNumber = 12;
+var name = "Borys";
+var surname = "Kovba";
+var greetings = "Aloha";
 
 System.Console.WriteLine("{0}, {1} {2}, {3}.{4}", greetings, name, surname, groupName, orderNumber);
