3e924f784e9cb3a4c705b7e281cd6b0074d9f947
Valerii Kolesnik <valerii.k@hiveon.net>
2022-09-22 09:45:36 +0300

    Replace task for IA-04

--- task.R
+++
@@ -1,8 +0,0 @@
-group_number <- "IA-XX"
-order_number <- "00"
-
-name <- "First Name"
-surname <- "Last Name"
-greetings <- "Hello World"
-
-paste(greetings, order_number, name, surname)
---
+++ task.cs
@@ -0,0 +1,7 @@
+var groupName = "IA-XX";
+var orderNumber = -1;
+var name = "First Name";
+var surname = "Last Name";
+var greetings = "Hello world";
+
+System.Console.WriteLine("{0}, {1} {2}, {3}.{4}", greetings, name, surname, groupName, orderNumber);
