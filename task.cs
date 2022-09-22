3194e7d8a6ade94478235764599c963eef5538cb
Nikita <nikitagrishaenko@gmail.com>
2022-09-22 11:44:30 +0300

    comm

--- task.cs
+++ task.cs
@@ -2,6 +2,6 @@ var groupName = "IA-04";
 var orderNumber = 7;
 var name = "Nikita";
 var surname = "Hryshaienko";
-var greetings = "Hello Ktlin";
+var greetings = "Hello Kotlin!!!";
 
 System.Console.WriteLine("{0}, {1} {2}, {3}.{4}", greetings, name, surname, groupName, orderNumber);
