6a1098fb28bf6e722fe8e284c16536aa92b9d643
ashonuyak <ashonuyak@gmail.com>
2022-09-22 11:07:21 +0300

    alo_tuta

--- task.cs
+++ task.cs
@@ -1,7 +1,7 @@
 var groupName = "IA-04";
-var orderNumber = -1;
+var orderNumber = 17;
 var name = "Bohdan";
-var surname = "Karashchuk";
+var surname = "Mazur";
 var greetings = "Hello world";
 
 System.Console.WriteLine("{0}, {1} {2}, {3}.{4}", greetings, name, surname, groupName, orderNumber);
