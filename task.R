8296880aa23ad623712b700a7559567df82f6ad0
Yehor Hnitii <yhnitii@osdb.io>
2022-09-22 09:16:26 +0300

    3 try

--- task.R
+++ task.R
@@ -1,13 +1,4 @@
 group_number <- "IA-01"
-order_number <- "17"
-
-name <- "Maksym"
-surname <- "Martiuk"
-greetings <- "Hello World"
-
-paste(greetings, order_number, name, surname)
-
-group_number <- "IA-01"
 order_number <- "05"
 
 name <- "Yehor"
