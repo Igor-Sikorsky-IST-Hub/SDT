421a1362ad405674acd680ea64484484531ee139
lilzem <91691062+lilzem@users.noreply.github.com>
2022-09-22 11:43:40 +0300

    my commit

--- task.R
+++
@@ -1,8 +0,0 @@
-group_number <- "IA-04"
-order_number <- "9"
-
-name <- "Roman"
-surname <- "Dmytrynskyi"
-greetings <- "Hello World"
-
-paste(greetings, order_number, name, surname)
---
+++ task.cs
@@ -0,0 +1,7 @@
+var groupName = "IA-04";
+var orderNumber = 9;
+var name = "Roman";
+var surname = " Dmytrynskyi";
+var greetings = "Hi";
+
+System.Console.WriteLine("{0}, {1} {2}, {3}.{4}", greetings, name, surname, groupName, orderNumber);
