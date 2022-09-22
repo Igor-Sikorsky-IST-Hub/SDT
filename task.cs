28e6a7995f4fb57bd327c89f39c2f08cdcee9972
Anastasiia <skotarenko.asya09@gmail.com>
2022-09-22 11:59:29 +0300

    my second commite

--- task.cs
+++ task.cs
@@ -1,7 +1,7 @@
 var groupName = "IA-04";
-var orderNumber = 6;
-var name = "Yuliia";
-var surname = "Hlushko";
+var orderNumber = 25;
+var name = "Skotarenko";
+var surname = "Anastasiia";
 var greetings = "Hi!!";
 
 System.Console.WriteLine("{0}, {1} {2}, {3}.{4}", greetings, name, surname, groupName, orderNumber);
