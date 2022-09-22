6ca93b824076bb94da786e14262009efa9815b42
Mykyta <nikitochkaaaaaa@gmail.com>
2022-09-22 11:09:21 +0300

    lab 1

--- task.cs
+++ task.cs
@@ -1,7 +1,7 @@
-var groupName = "IA-XX";
-var orderNumber = -1;
-var name = "First Name";
-var surname = "Last Name";
+var groupName = "IA-01";
+var orderNumber = -21;
+var name = "Mykyta";
+var surname = "Onatskyi";
 var greetings = "Hello world";
 
 System.Console.WriteLine("{0}, {1} {2}, {3}.{4}", greetings, name, surname, groupName, orderNumber);
