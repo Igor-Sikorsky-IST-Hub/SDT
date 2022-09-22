91930c510cd0ef970ad9d05dad0ba091914cadc3
Oleksandra Dron <dronsashka@gmail.com>
2022-09-22 09:53:59 +0300

    dron

--- task.R
+++ task.R
@@ -1,8 +1,8 @@
 group_number <- "IA-01"
-order_number <- "19"
+order_number <- "28"
 
-name <- "Rostyslav"
-surname <- "Nazarenko"
+name <- "Oleksandra"
+surname <- "Dron"
 greetings <- "Hello World"
 
 paste(greetings, order_number, name, surname)
