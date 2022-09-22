b8ff748315dc6c4daa10a04a398594f7cf136d52
gamedevlolkoven <lolkoven@gmail.com>
2022-09-22 09:27:43 +0300

    my change

--- task.R
+++ task.R
@@ -1,17 +1,8 @@
 group_number <- "IA-01"
 order_number <- "17"
 
-name <- "Maksym"
-surname <- "Martiuk"
-greetings <- "Hello World"
-
-paste(greetings, order_number, name, surname)
-
-group_number <- "IA-01"
-order_number <- "05"
-
-name <- "Yehor"
-surname <- "Hnitii"
+name <- "Daniil"
+surname <- "Zhyhorin"
 greetings <- "Hello World"
 
 paste(greetings, order_number, name, surname)
