4186e474fbf91a8c65189cd32e0a3948b0b16991
Boros <72230932+borosias@users.noreply.github.com>
2022-09-22 09:16:52 +0300

    Bohdan Hurov lab-1

--- task.R
+++ task.R
@@ -1,18 +1,8 @@
 group_number <- "IA-01"
-<<<<<<< HEAD
-order_number <- "06"
+order_number <- "07"
 
-name <- "Misha"
-surname <- "Hontar"
-greetings <- "Hello World and Misha"
-
-paste(greetings, order_number, name, surname)
-
-group_number <- "IA-01"
-order_number <- "05"
-
-name <- "Yehor"
-surname <- "Hnitii"
-greetings <- "Hello World"
+name <- "Bohdan"
+surname <- "Hurov"
+greetings <- "Glory to Ukraine"
 
 paste(greetings, order_number, name, surname)
